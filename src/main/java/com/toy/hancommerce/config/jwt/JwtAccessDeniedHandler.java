04571968a719073462ce4c59d8c589ddff3c3e17
gkstjr <gkstjr8332@naver.com>
2024-01-26 22:05:41 +0900

    JWT 기능 구현(설정부분 로그 기록하면서 순서 파악하면 좋을 듯)

--- src/main/java/com/toy/hancommerce/config/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/toy/hancommerce/config/jwt/JwtAccessDeniedHandler.java
@@ -3,16 +3,21 @@ package com.toy.hancommerce.config.jwt;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-
-@Component
+@Slf4j
+@Component //필요한 권한이 존재하지 않는 경우에 403 Forbidden 에러를 리턴하는 class
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.info("======================Security - JwtAccessDeniedHandler-handle=====================");
 
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
     }
 }
--- src/main/java/com/toy/hancommerce/config/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/toy/hancommerce/config/jwt/JwtAuthenticationEntryPoint.java
@@ -3,16 +3,21 @@ package com.toy.hancommerce.config.jwt;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-
-@Component
+@Slf4j
+@Component // 유효한 자격증명을 제공하지 않고 접근하려 할때 401 Unauthorized 에러를 리턴하는 class
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        //유효한 자격증명을 제공하지 않고 접근하려 할 때 401 응답
+        log.info("======================Security - JwtAuthenticationEntryPoint-commence=====================");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }

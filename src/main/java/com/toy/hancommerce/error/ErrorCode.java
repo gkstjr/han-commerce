bc679b35875142fd4106e8c0b36020f95099805a
gkstjr <gkstjr8332@naver.com>
2024-02-01 23:15:56 +0900

    ExceptionHandler 관련 개념 학습 및 적용

--- src/main/java/com/toy/hancommerce/error/ErrorCode.java
+++ src/main/java/com/toy/hancommerce/error/ErrorCode.java
@@ -11,7 +11,7 @@ public enum ErrorCode {
     USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND,"유저를 찾을 수 없습니다."),
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "유저명이 중복됩니다"),
     VALIDATION_ERROR(HttpStatus.BAD_REQUEST,"유효성 검증 실패"),
-
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"서버 에러"),
     //security 필터 단에서 뱉어내느 예외 코드
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"권한 없음"),
     UNAUTHENTICATED(HttpStatus.UNAUTHORIZED, "인증 실패");

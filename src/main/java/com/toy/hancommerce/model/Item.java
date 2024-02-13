96aaa5195e8afba842a4727ca1fc654b97f3625b
gkstjr <gkstjr8332@naver.com>
2024-02-13 17:54:30 +0900

    아이템 엔티티 설정

---
+++ src/main/java/com/toy/hancommerce/model/Item.java
@@ -0,0 +1,39 @@
+package com.toy.hancommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    private String name;
+
+    private long price;
+
+    private long stockQuantity;
+
+    private String content;
+
+    //연관관계 메소드(동작원리 주석처리하면서 이해하기)
+//    public void setCategory(Category category) {
+//        this.category = category;
+//        category.getItems().add(this);
+//    }
+
+}

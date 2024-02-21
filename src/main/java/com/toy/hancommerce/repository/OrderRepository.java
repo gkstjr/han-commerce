1c60a337bbfaa927e011947b1e361d39a5a633e7
gkstjr <gkstjr8332@naver.com>
2024-02-21 23:44:49 +0900

    배달 기능 구현 중

--- src/main/java/com/toy/hancommerce/repository/OrderRepository.java
+++ src/main/java/com/toy/hancommerce/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +14,7 @@ public interface OrderRepository extends JpaRepository<Order,Long> , OrderReposi
 
     @Query("select o from Order o join fetch o.delivery  join fetch o.orderItems where o.id = :id" )
     Optional<Order> findOneWithDeliveryOrderItemById(long id);
+
+    @Query("select o from Order o join fetch o.delivery where o.user.username = :username")
+    List<Order> findOrdersWithDeliverysByUsername(String username);
 }
